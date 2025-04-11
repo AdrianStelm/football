69ce90cde485264fb31cd381951719bf79f8c91d
vladislav <vladloz435@gmail.com>
2025-04-12 00:59:04 +0300

    change sematic

--- client/src/public/js/main.js
+++ client/src/public/js/main.js
@@ -1,11 +1,11 @@
-document.getElementById('changeImageBtn').addEventListener('click', function() {
-    // Получаем элемент изображения на кнопке
-    var buttonImage = document.getElementById('buttonImage');
-    
-    // Меняем источник изображения на кнопке
-    if (buttonImage.src.includes('../icons/Component 2.svg')) {
-        buttonImage.src = '../icons/images.png'; // Новое изображение
-    } else {
-        buttonImage.src = '../icons/Component 2.svg'; // Исходное изображение
+const buttons = document.querySelectorAll('.likeButton');
+buttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        button.classList.toggle('liked');
     }
-});
+)
+}
+)
+document.querySelector('.burger').addEventListener('click', function(){
+    this.classList.toggle('active');
+})
